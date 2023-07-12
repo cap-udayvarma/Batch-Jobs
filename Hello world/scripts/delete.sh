9a40ef6a12d152e62b9d7972b45c6356a7880f90
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-12 09:39:31 +0530

    Delete Hello world directory

--- Hello world/scripts/delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Directory where the files are located
-DIRECTORY="/mnt/c/Users/upandeti/Desktop/csv/"
-
-# Find files older than 30 days and delete them
-find "$DIRECTORY" -type f -mtime +10 -exec rm {} \;
-
-
