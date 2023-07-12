6d584a8cfd81d91f8e205b19adf2738e98c1eb23
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-13 00:06:38 +0530

    Add files via upload

---
+++ Batch-job-22/bin/load_fdbRaw_database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Specify the paths and filenames
+zip_file="/home/udayvarma/Batch-jobs/Batch-job-21/source/MKF44UPD.zip"
+unzip_dir="/home/udayvarma/Batch-jobs/Batch-job-21/destination/"
+sql_file="/home/udayvarma/Batch-jobs/Batch-job-21/destinatin/MKF44UPD/file.sql"
+database_name="sample1"
+database_user="root"
+database_password="uday"
+
+# Unzip the file
+unzip "$zip_file" -d "$unzip_dir"
+
+# Run the SQL file in the database
+mysql -u "$database_user" -p"$database_password" "$database_name" < "$sql_file"
