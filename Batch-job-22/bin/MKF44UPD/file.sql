12e219131a9492a40f4547bf021315fd57ade3c2
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-12 22:06:52 +0530

    Add files via upload

---
+++ Batch-job-22/bin/MKF44UPD/file.sql
@@ -0,0 +1,8 @@
+use sample1;
+create table demo
+(id INT ,
+username VARCHAR(100),
+password varchar(100));
+
+insert into demo values (101,'udajhvd','jasd');
+insert into demo values (103,'skjx','jasd'); 
