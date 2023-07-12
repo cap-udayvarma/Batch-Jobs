1a34a20614c9b9110673fd9dc2292cbac7458a68
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-13 00:06:12 +0530

    Delete Batch-job-22 directory

--- Batch-job-22/bin/MKF44UPD/file.sql
+++
@@ -1,8 +0,0 @@
-use sample1;
-create table demo
-(id INT ,
-username VARCHAR(100),
-password varchar(100));
-
-insert into demo values (101,'udajhvd','jasd');
-insert into demo values (103,'skjx','jasd'); 
