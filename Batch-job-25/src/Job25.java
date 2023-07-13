27670aaa8769688ce653ee103399454028c9c3a5
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-13 12:56:56 +0530

    Add files via upload

---
+++ Batch-job-25/src/Job25.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class Job25 {
+
+	public static void main(String[] args) {
+		try {
+        	String scriptPath = "/home/udayvarma/Batch-jobs/Batch-job-25/scripts/getHMS.sh";
+        	
+            ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
+            
+            
+            Process process = processBuilder.start();
+            
+           
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                System.out.println(" successfully.");
+            } else {
+                System.out.println("Failed .");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+	}
+
+}
