fce10ab09501a4147030eef0172e0b748b36fbc8
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-17 21:14:53 +0530

    Update Job78.java

--- Batch-Job-78/src/Job78.java
+++ Batch-Job-78/src/Job78.java
@@ -4,7 +4,7 @@ public class Job78 {
 
 	public static void main(String[] args) {
 		try {
-        	String scriptPath = "/home/udayvarma/Batch-jobs/Batch-job-78/scripts/UpdateUserInfo_Central.sh";
+        	String scriptPath = "/home/udayvarma/Batch-jobs/Batch-Job-78/scripts/UpdateUserInfo_Central.sh";
         	
             ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
             
