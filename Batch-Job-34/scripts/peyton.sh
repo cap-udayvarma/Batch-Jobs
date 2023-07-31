6d50bbc27de602e4c26bff0a2ea605311e9e8d26
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-07-31 23:01:58 +0530

    Update peyton.sh

--- Batch-Job-34/scripts/peyton.sh
+++ Batch-Job-34/scripts/peyton.sh
@@ -6,35 +6,23 @@ DB_USER="udayvarma"
 DB_PASSWORD="@Capgemini"
 DB_NAME="job34"
 
-# Folder to store the data dump file
-DATA_DUMP_FOLDER="/path/to/your/dump_folder"
+csv_file="*/files/product_cost.csv"
 
-# File to store the extracted data
-OUTPUT_FILE="$DATA_DUMP_FOLDER/data_dump.sql"
+# Database table name
+store_table_name="store"
 
-# Command to extract data from the database
-# Note: The use of mysqldump here, assuming you're using MySQL. Modify accordingly for other databases.
-mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$OUTPUT_FILE"
 
-# Check if the data dump file was created successfully
-if [ -f "$OUTPUT_FILE" ]; then
-    echo "Data extraction successful. File saved: $OUTPUT_FILE"
-else
-    echo "Error: Data extraction failed."
-    exit 1
-fi
+# Read the CSV file and insert data into the database table
+mysql -h "$db_host" -u "$db_user" -p"$db_password" "$db_store" <<EOF
+LOAD DATA LOCAL INFILE '$csv_file'
+INTO TABLE $store_table_name
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+EOF
 
-# Destination folder to move the data dump file
-DESTINATION_FOLDER="/path/to/your/destination_folder"
-
-# Move the data dump file to the destination folder
-mv "$OUTPUT_FILE" "$DESTINATION_FOLDER"
-
-# Check if the file was moved successfully
 if [ $? -eq 0 ]; then
-    echo "Data dump file moved to: $DESTINATION_FOLDER"
+  echo "Data inserted into the database table successfully."
 else
-    echo "Error: Failed to move the data dump file."
+  echo "Error inserting data into the database table."
 fi
-
-exit 0
