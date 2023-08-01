7282d21261013325656ca88902cbfc443e582e92
ashwinir2424 <92912289+ashwinir2424@users.noreply.github.com>
2023-08-01 15:01:51 +0530

    Add files via upload

---
+++ Group-3Batch_Jobs2/AZFunctionBatchJob41/AZFunctionBatchJob41/script/precheck_mailer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Function to send the email
+send_email() {
+    echo "Email sent successfully."
+}
+# Get the file path for the provided date or yesterday's date
+get_file_path() {
+    #local input_date="$1"
+    local input_date="20230721"
+    echo "Given date file is: $input_date"
+    if [ -z "$input_date" ]; then
+        # If no date is provided, use yesterday's date
+        input_date=$(date -d "yesterday" +%Y%m%d)
+        echo "Recived Yesterday File"
+    fi
+    # Assuming your precheck files are in the 'precheck_files' directory
+    local file_path="/mnt/c/Test-SB/Kroger/shellScript_20723/Jobs_workspace/BatchJob41/source/precheck_${input_date}.txt"
+    if [ ! -f "$file_path" ]; then
+        echo "Error: The precheck file for date $input_date does not exist."
+        exit 1
+    fi
+    echo "$file_path"
+}
+# Main script starts here
+# Define the mailer list recipients (comma-separated emails)
+#mailer_list="av50111@gmail.com,ashwinir153@yahoo.com"
+# Get the input date from the command-line argument (if provided)
+#input_date="$1"
+# Get the file path for the input date
+#file_path=$(get_file_path "$input_date")
+# Send the precheck file to the mailer list
+get_file_path
+send_email
---
+++ Group-3Batch_Jobs2/AZFunctionBatchJob41/AZFunctionBatchJob41/src/main/java/com/job/TimerTriggerFunction.java
@@ -0,0 +1,24 @@
+package com.job;
+
+import java.time.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Timer trigger.
+ */
+public class TimerTriggerFunction {
+    /**
+     * This function will be invoked periodically according to the specified schedule.
+     */
+    @FunctionName("TimerTrigger-Java")
+    public void run(
+        @TimerTrigger(name = "timerInfo", schedule = "0 * * * * *") String timerInfo,
+        final ExecutionContext context
+    ) {
+    	PrecheckMailer precheckMailer = new PrecheckMailer();
+		precheckMailer.PrecheckMailerFU();
+		System.out.println("Inside time trigger");
+        context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+    }
+}
