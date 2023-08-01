2deb44630469d1bbc45a11e5e58ca94c1e2d3764
ashwinir2424 <92912289+ashwinir2424@users.noreply.github.com>
2023-08-01 17:43:36 +0530

    Add files via upload

---
+++ BatchJob_3/src/StageCardinal.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+
+public class StageCardinal {
+	public static void main(String[] args) {
+		StageCardinal stageCardinal = new StageCardinal();
+		stageCardinal.StageCardinalFi();
+	}
+		public void StageCardinalFi() {
+		try {
+			String scriptPath = "/home/amith/batchjobs/Batch-Job-3/scripts/stageCardinal.sh";
+
+			ProcessBuilder processBuilder = new ProcessBuilder("bash", scriptPath);
+
+			Process process = processBuilder.start();
+
+			int exitCode = process.waitFor();
+
+			if (exitCode == 0) {
+				System.out.println(" successfully.");
+			} else {
+				System.out.println("Failed .");
+			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
