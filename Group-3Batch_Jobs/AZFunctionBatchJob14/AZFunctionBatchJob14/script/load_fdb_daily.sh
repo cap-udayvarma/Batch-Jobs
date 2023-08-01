c2a57c69333c4171cefcaaa16411d4ba9d7d08d3
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-08-01 17:17:09 +0530

    Update load_fdb_daily.sh

--- Group-3Batch_Jobs/AZFunctionBatchJob14/AZFunctionBatchJob14/script/load_fdb_daily.sh
+++ Group-3Batch_Jobs/AZFunctionBatchJob14/AZFunctionBatchJob14/script/load_fdb_daily.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Script is start"
-awk -F',' '{print $0}' /mnt/c/Test-SB/Kroger/shellScript_20723/Jobs_workspace/BatchJob14/source/FDB_Daily_price_updates.csv
+awk -F',' '{print $0}' /home/udayvarma/Batch-jobs/Group-3Batch_Jobs/AZFunctionBatchJob14/AZFunctionBatchJob14/source/FDB_Daily_price_updates.csv
 #source /mnt/c/Users/AMBATS/Desktop/Batchjob/SourceFile/FDB_Daily_price_updates.csv
 echo "Script is running"
