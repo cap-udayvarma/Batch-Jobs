a2609104a4f21334aaa902954088d5510cac64c5
ashwinir2424 <92912289+ashwinir2424@users.noreply.github.com>
2023-08-02 17:53:39 +0530

    Add files via upload

---
+++ Group3-BatchJob_Trigger_41/src/main/java/com/example/TimerTriggerFunction.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.time.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Timer trigger.
+ */
+public class TimerTriggerFunction {
+	/**
+	 * This function will be invoked periodically according to the specified
+	 * schedule.
+	 */
+	@FunctionName("TimerTrigger-Java")
+	public void run(@TimerTrigger(name = "timerInfo", schedule = "30 * * * * *") String timerInfo,
+			final ExecutionContext context) {
+
+		String vmIpAddress = "20.106.243.180";
+
+		String vmUsername = "udayvarma";
+
+		String vmPassword = "@Uday9533596417";
+
+		String commandToExecute = "cd /home/udayvarma/Batch-jobs/BatchJob_41/src && java PrecheckMailer";
+
+		// Execute the command on the VM using SSH
+
+		try {
+
+			SSHUtil.executeCommand(vmIpAddress, vmUsername, vmPassword, commandToExecute);
+
+		} catch (Exception e) {
+
+			context.getLogger().severe("Failed to execute Java project on VM: " + e.getMessage());
+
+		}
+		{
+			context.getLogger().info("Java Timer trigger function executed at: " + LocalDateTime.now());
+		}
+	}
+}
