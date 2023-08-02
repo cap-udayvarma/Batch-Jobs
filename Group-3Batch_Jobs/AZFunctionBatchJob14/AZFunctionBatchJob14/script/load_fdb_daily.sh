e92fba2a0224f66535bdef79bb3bd06e3b4ce69c
udayvarma0007 <32445790+udayvarma0007@users.noreply.github.com>
2023-08-02 17:35:23 +0530

    Delete Group-3Batch_Jobs directory

--- Group-3Batch_Jobs/AZFunctionBatchJob14/AZFunctionBatchJob14/script/load_fdb_daily.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Script is start"
-awk -F',' '{print $0}' /home/udayvarma/Batch-jobs/Group-3Batch_Jobs/AZFunctionBatchJob14/AZFunctionBatchJob14/source/FDB_Daily_price_updates.csv
-#source /mnt/c/Users/AMBATS/Desktop/Batchjob/SourceFile/FDB_Daily_price_updates.csv
-echo "Script is running"
